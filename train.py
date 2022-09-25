fbe2cb754191192eefd984b0b066b463047d0f3a
gkgo <91817417+gkgo@users.noreply.github.com>
2022-09-25 20:33:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def train(epoch, model, loader, optimizer, args=None):
     train_loader_aux = loader['train_loader_aux']
 
     # label for query set, always in the same pattern
-    label = torch.arange(args.way).repeat(args.query).flip(dims=[0]).cuda()  # 432104321043210....
+    label = torch.arange(args.way).repeat(args.query).cuda()  # 432104321043210....
 
 
     loss_meter = Meter()
