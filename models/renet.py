b323ae975cff7201fc5f6855e5299ffe47352f8a
gkgo <91817417+gkgo@users.noreply.github.com>
2023-08-12 17:45:41 +0800

    Update renet.py

--- models/renet.py
+++ models/renet.py
@@ -1,18 +1,7 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
 from models.resnet import ResNet
+from models.ca import *
+from models.sa import *
 from models.resnet18 import resnet18
-from models.cca import *
-from models.scr import  *
-import numpy as np
-# from models.others.se import SqueezeExcitation
-# from models.others.lsa import LocalSelfAttention
-# from models.others.nlsa import NonLocalSelfAttention
-# from models.others.sce import SpatialContextEncoder
-
 
 class RENet(nn.Module):
 
@@ -24,57 +13,26 @@ class RENet(nn.Module):
         # self.encoder = ResNet(args=args)
         self.encoder = resnet18(args=args)
         self.encoder_dim = 640
-
         self.fc = nn.Linear(self.encoder_dim, self.args.num_class)
-        self.scr_module = self._make_scr_layer(planes=[640, 64, 64, 64, 640])
+        self.scr_module = self._make_scr_layer()
         self.match_net = match_block(640)
-        self.match_net1 = match_block1(640)
-        self.match_net2 = SpatialContextEncoder(planes=[640, 64, 64, 640], kernel_size=5)
-        self.cca_module = CCA(kernel_sizes=[3, 3], planes=[16, 1])
         self.cca_1x1 = nn.Sequential(
             nn.Conv2d(self.encoder_dim, 64, kernel_size=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU()
         )
 
-
-
-    def _make_scr_layer(self, planes):
-        stride, kernel_size, padding = (1, 1, 1), (5, 5), 2
+    def _make_scr_layer(self):
+        kernel_size, padding = (5, 5), 2
         layers = list()
 
-        if self.args.self_method == 'scr':
-#             corr_block2 = SelfCorrelationComputation1(d_model=640, h=1) #self
-#             corr_block2 = SelfCorrelationComputation4(channel=640)  # se
-            corr_block2 = mySelfCorrelationComputation(kernel_size=kernel_size, padding=padding)
-#             corr_block2 = SpatialContextEncoder(planes=[640, 64, 64, 640], kernel_size=kernel_size[0])  #sce
-#             corr_block = SelfCorrelationComputation(kernel_size=kernel_size, padding=padding)
-#             self_block = SCR(planes=planes, stride=stride)
-#             corr_block2 = SelfCorrelationComputation9(channel=640)  # CA
-#             corr_block2 = SelfCorrelationComputation6(in_planes=640, out_planes=640)  # local
-#             corr_block2 = SelfCorrelationComputation5(in_channels=640, out_channels=640)   # gam
-#             corr_block2 = SelfCorrelationComputation8(channels=640)  # nam
-#             corr_block2 = SelfCorrelationComputation12(channel=640) # cbam
-#             corr_block2 = SelfCorrelationComputation3(in_channels=640) # nonl
-#             corr_block2 = SelfCorrelationComputation13(channel=640)   # eca
-#             corr_block2 = SelfCorrelationComputation10(dim=640, kernel_size=5, num_heads=1) # nat
-            # corr_block2 = SelfCorrelationComputation2(in_channels=640, out_channels=640, kernel_size=5)
-        # elif self.args.self_method == 'sce':
-        #     planes = [640, 64, 64, 640]
-        #     self_block = SpatialContextEncoder(planes=planes, kernel_size=kernel_size[0])
-        # elif self.args.self_method == 'se':
-        #     self_block = SqueezeExcitation(channel=planes[0])
-        # elif self.args.self_method == 'lsa':
-        #     self_block = LocalSelfAttention(in_channels=planes[0], out_channels=planes[0], kernel_size=kernel_size[0])
-        # elif self.args.self_method == 'nlsa':
-        #     self_block = NonLocalSelfAttention(planes[0], sub_sample=False)
+        if self.args.self_method == 'sa':
+            corr_block = mySelfCorrelationComputation(kernel_size=kernel_size, padding=padding)
         else:
             raise NotImplementedError
 
-        if self.args.self_method == 'scr':
-            layers.append(corr_block2)
-#             layers.append(corr_block)
-#         layers.append(self_block)
+        if self.args.self_method == 'sa':
+            layers.append(corr_block)
         return nn.Sequential(*layers)
 
     def forward(self, input):
@@ -82,9 +40,9 @@ class RENet(nn.Module):
             return self.fc_forward(input)
         elif self.mode == 'encoder':
             return self.encode(input, False)
-        elif self.mode == 'cca':
+        elif self.mode == 'ca':
             spt, qry = input
-            return self.cca(spt, qry)
+            return self.ca(spt, qry)
         else:
             raise ValueError('Unknown mode')
 
@@ -92,16 +50,16 @@ class RENet(nn.Module):
         x = x.mean(dim=[-1, -2])
         return self.fc(x)
 
-    def cca(self, spt, qry):  # 支持，查询
+    def ca(self, spt, qry):  # 支持，查询
 
         spt = spt.squeeze(0)  # 移除数组中维度为1的维度
 
         spt = self.normalize_feature(spt)  # 1
         qry = self.normalize_feature(qry)
-#_________________________________________________________________________________baseline
+        # _________________________________________________________________________________baseline
         # way = spt.shape[0]
         # num_qry = qry.shape[0]
-        # H_s, W_s, H_q, W_q = 11,11,11,11
+        # H_s, W_s, H_q, W_q = 5,5,5,5
         # spt_c = F.normalize(spt, p=2, dim=1, eps=1e-8)
         # qry_c = F.normalize(qry, p=2, dim=1, eps=1e-8)
         # # way , C , H_s , W_s --> num_qry * way, C , H_s , W_s
@@ -112,29 +70,29 @@ class RENet(nn.Module):
         # d_q = qry_c.view(num_qry, way,640,H_q*W_q)  # 10，5，5，5，25
         # d_s = self.gaussian_normalize(d_s, dim=3)
         # d_q = self.gaussian_normalize(d_q, dim=3)
-        
+        #
         # # applying softmax for each side
         # d_s = F.softmax(d_s / self.args.temperature_attn, dim=3)
         # d_s = d_s.view(num_qry, way,640,H_s, W_s)  # 10，5，5，5，5，5
         # d_q = F.softmax(d_q / self.args.temperature_attn, dim=3)
         # d_q = d_q.view(num_qry, way,640,H_q, W_q)  # 10，5，5，5，5，5
-        
+        #
         # spt_attended = d_s * spt.unsqueeze(0)  # 10，5，640，5，5
         # qry_attended = d_q * qry.unsqueeze(1)  # 10，5，640，5，5
 
-#_____________________________________________________________________________________
+        # _____________________________________________________________________________________
         way = spt.shape[0]
         num_qry = qry.shape[0]
         H_s, W_s, H_q, W_q = 11,11,11,11
         spt_attended1, qry_attended1 = self.match_net(spt, qry)  # 先 Channel
-        spt_attended1 = spt_attended1.view(num_qry, way,640,H_s, W_s)
-        qry_attended1 = qry_attended1.view(num_qry, way,640,H_q, W_q)
+        spt_attended1 = spt_attended1.view(num_qry, way, 640, H_s, W_s)
+        qry_attended1 = qry_attended1.view(num_qry, way, 640, H_q, W_q)
 
         spt_attended1 = F.relu(spt_attended1, inplace=True)
         qry_attended1 = F.relu(qry_attended1, inplace=True)
 
-        d_s = spt_attended1.view(num_qry, way,640,H_s*W_s)  # 10，5，25，5，5
-        d_q = qry_attended1.view(num_qry, way,640,H_q*W_q)  # 10，5，5，5，25
+        d_s = spt_attended1.view(num_qry, way, 640, H_s * W_s)  # 10，5，25，5，5
+        d_q = qry_attended1.view(num_qry, way, 640, H_q * W_q)  # 10，5，5，5，25
 
         # normalizing the entities for each side to be zero-mean and unit-variance to stabilize training
         d_s = self.gaussian_normalize(d_s, dim=3)
@@ -142,40 +100,12 @@ class RENet(nn.Module):
 
         # applying softmax for each side
         d_s = F.softmax(d_s / self.args.temperature_attn, dim=3)
-        d_s = d_s.view(num_qry, way,640,H_s, W_s)  # 10，5，5，5，5，5
+        d_s = d_s.view(num_qry, way, 640, H_s, W_s)  # 10，5，5，5，5，5
         d_q = F.softmax(d_q / self.args.temperature_attn, dim=3)
-        d_q = d_q.view(num_qry, way,640,H_q, W_q)  # 10，5，5，5，5，5
+        d_q = d_q.view(num_qry, way, 640, H_q, W_q)  # 10，5，5，5，5，5
 
         spt_attended = d_s * spt.unsqueeze(0)  # 10，5，640，5，5
         qry_attended = d_q * qry.unsqueeze(1)  # 10，5，640，5，5
-——————————————————————————————————————————————————————————————————————————————————————————————
-      (S * C * Hs * Ws, Q * C * Hq * Wq) -> Q * S * Hs * Ws * Hq * Wq
-        corr4d = self.get_4d_correlation_map(spt, qry)
-        num_qry, way, H_s, W_s, H_q, W_q = corr4d.size()
-
-        # corr4d refinement
-#         corr4d = self.cca_module(corr4d.view(-1, 1, H_s, W_s, H_q, W_q))
-        corr4d_s = corr4d.view(num_qry, way, H_s * W_s, H_q, W_q)
-        corr4d_q = corr4d.view(num_qry, way, H_s, W_s, H_q * W_q)
-
-        # normalizing the entities for each side to be zero-mean and unit-variance to stabilize training
-        corr4d_s = self.gaussian_normalize(corr4d_s, dim=2)
-        corr4d_q = self.gaussian_normalize(corr4d_q, dim=4)
-
-        # applying softmax for each side
-        corr4d_s = F.softmax(corr4d_s / self.args.temperature_attn, dim=2)
-        corr4d_s = corr4d_s.view(num_qry, way, H_s, W_s, H_q, W_q)
-        corr4d_q = F.softmax(corr4d_q / self.args.temperature_attn, dim=4)
-        corr4d_q = corr4d_q.view(num_qry, way, H_s, W_s, H_q, W_q)
-
-        # suming up matching scores
-        attn_s = corr4d_s.sum(dim=[4, 5])
-        attn_q = corr4d_q.sum(dim=[2, 3])
-
-        # applying attention
-        spt_attended = attn_s.unsqueeze(2) * spt.unsqueeze(0)
-        qry_attended = attn_q.unsqueeze(2) * qry.unsqueeze(1)
-
 
         # averaging embeddings for k > 1 shots
         if self.args.shot > 1:
@@ -196,7 +126,6 @@ class RENet(nn.Module):
         else:
             return similarity_matrix / self.args.temperature
 
-
 # ----------------------------------------------------------------------------------
     def gaussian_normalize(self, x, dim, eps=1e-05):
         x_mean = torch.mean(x, dim=dim, keepdim=True)
@@ -204,47 +133,18 @@ class RENet(nn.Module):
         x = torch.div(x - x_mean, torch.sqrt(x_var + eps))  # （x原始-x平均）/根号下x_var
         return x
 
-    def get_4d_correlation_map(self, spt, qry):
-        '''
-        The value H and W both for support and query is the same, but their subscripts are symbolic.
-        :param spt: way * C * H_s * W_s
-        :param qry: num_qry * C * H_q * W_q
-        :return: 4d correlation tensor: num_qry * way * H_s * W_s * H_q * W_q
-        :rtype:
-        '''
-        way = spt.shape[0]
-        num_qry = qry.shape[0]
-
-        # reduce channel size via 1x1 conv
-        spt = self.cca_1x1(spt)
-        qry = self.cca_1x1(qry)
-
-        # normalize channels for later cosine similarity
-        spt = F.normalize(spt, p=2, dim=1, eps=1e-8)
-        qry = F.normalize(qry, p=2, dim=1, eps=1e-8)
-
-        # num_way * C * H_p * W_p --> num_qry * way * H_p * W_p
-        # num_qry * C * H_q * W_q --> num_qry * way * H_q * W_q
-        spt = spt.unsqueeze(0).repeat(num_qry, 1, 1, 1, 1)
-        qry = qry.unsqueeze(1).repeat(1, way, 1, 1, 1)
-        similarity_map_einsum = torch.einsum('qncij,qnckl->qnijkl', spt, qry)
-        return similarity_map_einsum
-
     def normalize_feature(self, x):
         return x - x.mean(1).unsqueeze(1)  # x-x.mean(1)行求平均值并在channal维上增加一个维度
 
     def encode(self, x, do_gap=True):
         x = self.encoder(x)
 
-#         if self.args.self_method:
-#             identity = x  # (80,640,5,5)
-#             x = self.scr_module(x)
-
-#             # x = self.match_net2(x,identity)
-
-#             if self.args.self_method == 'scr':
-#                 x = x + identity   # 公式（2）
-#             x = F.relu(x, inplace=True)
+        if self.args.self_method:
+            identity = x  # (80,640,5,5)
+            x = self.scr_module(x)
+            if self.args.self_method == 'sa':
+                x = x + identity
+            x = F.relu(x, inplace=True)
 
         if do_gap:
             return F.adaptive_avg_pool2d(x, 1)
