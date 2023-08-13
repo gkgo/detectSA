ab60a0f07468d52a43410c9cc8deb3c83910b616
gkgo <91817417+gkgo@users.noreply.github.com>
2023-08-13 08:42:21 +0800

    Update samplers.py

--- models/dataloader/samplers.py
+++ models/dataloader/samplers.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class CategoriesSampler():
 
-    def __init__(self, label, n_batch, n_cls,shot,n_per):
+    def __init__(self, label, n_batch, n_cls, shot, n_per):
         self.n_batch = n_batch  # the number of iterations in the dataloader
         self.n_cls = n_cls
         self.n_shot = shot * n_cls
@@ -19,7 +19,7 @@ class CategoriesSampler():
 
     def __len__(self):
         return self.n_batch
-    
+
     def __iter__(self):
         for i_batch in range(self.n_batch):
             batch = []
@@ -29,10 +29,10 @@ class CategoriesSampler():
                 pos = torch.randperm(len(l))[:self.n_per]  # sample n_per data index of this class
                 batch.append(l[pos])
             batch = torch.stack(batch).t().reshape(-1)
-#             batch1 = batch[0:self.n_shot]
-#             batch2 = batch[self.n_shot:]
-#             x = batch2.flip(dims=[0])
-#             batch = torch.cat((batch1,x), dim=0)
+            batch1 = batch[0:self.n_shot]
+            batch2 = batch[self.n_shot:]
+            x = batch2.flip(dims=[0])
+            batch = torch.cat((batch1, x), dim=0)
             # .t() transpose,
             # due to it, the label is in the sequence of abcdabcdabcd form after reshape,
             # instead of aaaabbbbccccdddd
